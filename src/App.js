d2d9d81bb5bad220582fc5245ed77abe0c0f36b3
catwhitmer <catwhitmer@gmail.com>
2020-09-02 08:41:23 -0400

    create initial App.js state

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      fetchMovies: true,
+      movieData: {}
+    }
+  }
   
   render() {
     return (
       <div className="App">
-        
+        <h1>What To Watch</h1>
       </div>
     );
   }
