647b6b7f887877b64b801ce66a3f631540452d12
catwhitmer <catwhitmer@gmail.com>
2020-09-02 09:59:45 -0400

    passed down props to Movies.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import React from 'react';
 import './App.css';
+import Movies from './components/Movies'
+
+const APIURL = `https://api.themoviedb.org/3/movie/now_playing?api_key=ff0c237eec536bb28173dfdf3bf21a40`
 
 class App extends React.Component {
   constructor() {
     super()
 
     this.state = {
-      fetchMovies: true,
-      movieData: {}
+      isLoaded: false,
+      results: {}
     }
   }
+
+  componentDidMount() {
+    fetch(APIURL)
+    .then(response => response.json())
+    .then(data => { 
+      this.setState ({
+        isLoaded: true, 
+        results: data.results 
+        })
+      }
+    )
+  }
+  
   
   render() {
+    const { isLoaded, results} = this.state
     return (
       <div className="App">
         <h1>What To Watch</h1>
+        <Movies results={this.state.results}/>
       </div>
     );
   }
---
+++ src/components/Movies.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Movies = (props) => {
+     
+    return (
+        <div>
+        Movie
+        </div>
+    )
+}
+
+export default (Movies)
