e899aa40c548250102d263c57e04de0d0ca2f548
catwhitmer <catwhitmer@gmail.com>
2020-09-14 08:40:57 -0400

    working on rendering movies

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -6,8 +6,8 @@ const Movies = (props) => {
     return (
         <div>
             <h2> Movies </h2>
-              {props.results && props.results.map(movie =>
-                    <li> {movie.title} </li>)}
+                {props.results.map(result => 
+                    <li> {result.title} - {result.backdrop_path} - {result.release_date} </li>)}
         </div>
     )
 }
