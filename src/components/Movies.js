8350c1a934a3a7193aafee129bc0489397ba8e9d
catwhitmer <catwhitmer@gmail.com>
2020-09-16 08:38:36 -0400

    did initial styling and displayed movie titles

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -3,16 +3,17 @@ import React from 'react'
 const APIURL = `https://api.themoviedb.org/3/movie/now_playing?api_key=ff0c237eec536bb28173dfdf3bf21a40`
 
 class Movies extends React.Component{
-   
-   state = {
-       results: {}
+   constructor(props) {
+    super(props);
+    this.state = {
+       results: []
+    }
    }
    
     componentDidMount() {
         fetch(APIURL)
         .then (resp => resp.json())
-        .then (
-            (data) => {
+        .then ((data) => {
             this.setState({
                 results: data.results
             })
@@ -21,10 +22,13 @@ class Movies extends React.Component{
     }
 
    render() {
-   
+       const { results} = this.state
         return (
             <div>
-                <h2> Movies </h2>   
+                <h2> Movies </h2> 
+                    {results.map(movie => (
+                        <li> {movie.title} </li>
+                    ))}
             </div>
         )
    }
