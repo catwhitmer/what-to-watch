9682e90c0ccd53fc395cee408386d449ce42550e
catwhitmer <catwhitmer@gmail.com>
2020-09-18 08:43:31 -0400

    converted Movies.js from traditional layout to using hooks

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,40 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const APIURL = `https://api.themoviedb.org/3/movie/now_playing?api_key=ff0c237eec536bb28173dfdf3bf21a40`
 
-class Movies extends React.Component{
-   constructor(props) {
-    super(props);
-    this.state = {
-       results: [],
-       movies: []
-    }
-   }
+
+function Movies() {
+ 
+    const [ results, setResults ] = useState([]);
    
-    componentDidMount() {
+    useEffect(() => {
         fetch(APIURL)
         .then (resp => resp.json())
         .then ((data) => {
-            this.setState({
-                results: data.results
-            })
-            }
-        )
-    }
+            setResults(data.results)
+        })
+    }, [])
 
- 
-
-   render() {
-       const { results } = this.state
-        return (
-            <div>
-                <h2> Movies </h2> 
-                    {results.map(movie => (
-                        <li key={movie.id}> {movie.title} </li>
-                    ))}
-            </div>
-        )
-   }
+    return (
+        <div>
+            <h2> Movies </h2> 
+                {results.map(movie => (
+                    <li key={movie.id}> {movie.title} </li>
+                ))}
+        </div>
+    )
 
 }
 
